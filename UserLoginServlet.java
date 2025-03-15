392631681aa7031b5cada9a753bd67ae984d5c1a
Ankit <iankitraj18@gmail.com>
2025-03-15 22:17:19 +0530

    Updated UserLoginServlet.java

--- UserLoginServlet.java
+++ UserLoginServlet.java
@@ -71,6 +71,7 @@ public class UserLoginServlet extends HttpServlet {
 	                if (con != null) con.close();
 	            } catch (Exception e) {
 	                e.printStackTrace();
+	                
 	            }
 	        }
 	    }
